9e4e3bb323eeaa75f6e4cbc4d345cf86fc727e2b
Jason Browne <jason.browne@uzh.ch>
2019-10-03 12:22:10 +0200

    hallo 3

--- TEST 2/src/Test2.java
+++ TEST 2/src/Test2.java
@@ -1,6 +1,10 @@
 public class Test2 {
 
 
+    public static void main(String[] args) {
 
-    //Da chunnt no min senf dezue hallo
+
+        //Da chunnt no min senf dezue hallo
+        System.out.println("Hallo");
+    }
 }
