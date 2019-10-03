fee26579afeb646222bfcc089442ee1d7baf0e3d
pineri <piero.neri99@bluewin.ch>
2019-10-03 15:14:38 +0200

    Test Gameboard

--- SnakesandLadders/src/Main.java
+++ SnakesandLadders/src/Main.java
@@ -1,7 +1,20 @@
 public class Main {
-    public static void main(String args[]){
+    public static void main(String args[]) {
+
+        StartSquare startsquare1 = new StartSquare(1);
+        Ladder ladder2 = new Ladder(2);
+        NormalSquare normalsquare3 = new NormalSquare(3);
+        NormalSquare normalsquare4 = new NormalSquare(4);
+        NormalSquare normalsquare5 = new NormalSquare(5);
+        NormalSquare normalsquare6 = new NormalSquare(6);
+        Ladder ladder7 = new Ladder(7);
+        NormalSquare normalsquare8 = new NormalSquare(8);
+        NormalSquare normalsquare9 = new NormalSquare(9);
+        NormalSquare normalsquare10 = new NormalSquare(10);
+        Snake snake11 = new Snake(11);
+        EndSquare endsquare12 = new EndSquare(12);
 
 
 
-
+    }
 }
