8c775fc23135e96772489f30244411b0ad1a92b9
davebasler <45432987+davebasler@users.noreply.github.com>
2019-10-03 12:48:00 +0200

    Project Launch

---
+++ SnakesandLadders/src/Dice.java
@@ -0,0 +1,5 @@
+public class Dice {
+    public static void getnumber(){
+        System.out.println("A number 1-6");
+    }
+}
---
+++ SnakesandLadders/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String args[]){
+        System.out.println("Snaked and Ladders Game");
+        Dice dice1 = new Dice();
+        dice1.getnumber();
+    }
+
+
+}
